5b6e9ea7f646edff9c0f3823090cebf9a2b1c6b4
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-25 21:01:01 +0100

    add global colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "./components/Button/Button";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>IMPERIUM_MOTUS</h1>
-      <Button type={"secondary"} label={"Test Button"} />
     </div>
   );
 }
