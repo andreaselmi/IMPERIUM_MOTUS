1891a2346ca6b89c5dd3df4da8084cb75653f5ba
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-26 19:30:04 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar/Navbar";
-import Container from "./components/Container/Container";
 import CalisthenicsSection from "./components/Section/CalisthenicsSection/CalisthenicsSection";
 
 function App() {
