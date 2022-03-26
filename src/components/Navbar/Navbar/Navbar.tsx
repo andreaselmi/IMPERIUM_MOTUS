0846ab3b0172fc6decfb4517426854bea245268a
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-26 17:21:38 +0100

    create custom hook for check window dimensions and add conditionally render navbar

--- src/components/Navbar/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import NavbarMobile from "../NavbarMobile/NavbarMobile";
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
+import NavbarDesktop from "../NavbarDesktop/NavbarDesktop";
 
 const Navbar = () => {
-  return <NavbarMobile />;
+  const { width } = useWindowDimensions();
+  return width && width > 991 ? <NavbarDesktop /> : <NavbarMobile />;
 };
 
 export default Navbar;
