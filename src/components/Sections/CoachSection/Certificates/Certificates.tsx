761e8bd9fdab9fa7fb3275d5ec38c6dbb5f290c3
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-27 18:11:35 +0200

    add responsive layout to coach section

---
+++ src/components/Sections/CoachSection/Certificates/Certificates.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Typography from "../../../Typography/Typography";
+import attestatoAICS from "../../../../assets/images/sections/attestato1.png";
+import attestatoFGI from "../../../../assets/images/sections/attestato2.png";
+import attestatoTrainingLab from "../../../../assets/images/sections/attestato3.png";
+import attestatoMidea from "../../../../assets/images/sections/attestato4.png";
+import styles from "./Certificates.module.css";
+
+const Certificates = () => {
+  return (
+    <div className={styles.certificatesScrollableContainer}>
+      <div className={styles.certificatesContainer}>
+        <Typography
+          style={{ marginBottom: 24 }}
+          variant={"heading"}
+          label={"Attestati"}
+        />
+        <div className={styles.certificatesContentContainer}>
+          <div className={styles.singleCertificateContainer}>
+            <div className={styles.certificateImgContainer}>
+              <img
+                src={attestatoAICS}
+                alt={"Logo Attestato Istruttore Calisthenico (AICS)"}
+              />
+            </div>
+            <Typography
+              className={styles.certificateText}
+              variant={"smallTitle"}
+              label={"Istruttore Allenamento Calisthenico (AICS)"}
+            />
+          </div>
+          <div className={styles.singleCertificateContainer}>
+            <div className={styles.certificateImgContainer}>
+              <img
+                style={{ height: 101, width: 101 }}
+                src={attestatoFGI}
+                alt={"Logo federazione Ginnastica Italiana"}
+              />
+            </div>
+            <Typography
+              className={styles.certificateText}
+              variant={"smallTitle"}
+              label={"Master “L’ ABC della GINNASTICA”"}
+            />
+          </div>
+          <div className={styles.singleCertificateContainer}>
+            <div className={styles.certificateImgContainer}>
+              <img
+                src={attestatoTrainingLab}
+                alt={"Logo Training Lab Italia"}
+              />
+            </div>
+            <Typography
+              className={styles.certificateText}
+              variant={"smallTitle"}
+              label={"Istruttore Functional Training"}
+            />
+          </div>
+          <div className={styles.singleCertificateContainer}>
+            <div className={styles.certificateImgContainer}>
+              <img src={attestatoMidea} alt={"Logo Midea"} />
+            </div>
+            <Typography
+              className={styles.certificateText}
+              variant={"smallTitle"}
+              label={"Istruttore Posturologia"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Certificates;
--- src/components/Sections/CoachSection/CoachSection.tsx
+++ src/components/Sections/CoachSection/CoachSection.tsx
@@ -6,114 +6,53 @@ import coachHeader from "../../../assets/images/sections/coachSection.svg";
 import coachImg from "../../../assets/images/sections/coach.jpg";
 import Typography from "../../Typography/Typography";
 import Button from "../../Button/Button";
-
-import attestatoAICS from "../../../assets/images/sections/attestato1.png";
-import attestatoFGI from "../../../assets/images/sections/attestato2.png";
-import attestatoTrainingLab from "../../../assets/images/sections/attestato3.png";
-import attestatoMidea from "../../../assets/images/sections/attestato4.png";
+import Certificates from "./Certificates/Certificates";
 
 const CoachSection = () => {
   return (
     <div className={styles.sectionContainer}>
       <SectionContainer>
-        <div className={styles.container}>
+        <div className={styles.headerContainer}>
           <SectionHeader
             imageSrc={coachHeader}
             imageAlt={"Il Coach"}
             label={"Alessandro Lupo"}
           />
+        </div>
 
-          <div className={styles.imageContainer}>
-            <img
-              className={styles.image}
-              alt={"Un immagine del coach Alessandro Lupo"}
-              src={coachImg}
-            />
-          </div>
-          <div className={styles.textContainer}>
-            <Typography
-              variant={"paragraph"}
-              label={
-                "Alessandro Lupo, classe 1992, prima di tutto un atleta e per passione e professione faccio il personal trainer. Nel mio passato ho fatto molti sport: karate, calcio, kung fu, pallavolo, golf e il tennis che tutt’ora è una grande passione."
-              }
-            />
-            <br />
-            <Typography
-              variant={"paragraph"}
-              label={
-                "Una sera del 2012 un amico mi mostrò un video di Frank Medrano, quel giorno scoprì la passione per il Calisthenics. Rimasi così impressionato da mollare immediatamente la sala pesi, il giorno dopo alle 8:00 di mattina ero già al parco per fare il mio primo allenamento."
-              }
-            />
+        <div className={styles.imageContainer}>
+          <img
+            className={styles.image}
+            alt={"Un immagine del coach Alessandro Lupo"}
+            src={coachImg}
+          />
+        </div>
 
-            <Button
-              className={styles.button}
-              label={"Leggi la mia storia"}
-              type={"primary"}
-            />
-          </div>
+        <div className={styles.textContainer}>
+          <Typography
+            className={styles.text}
+            variant={"paragraph"}
+            label={
+              "Alessandro Lupo, classe 1992, prima di tutto un atleta e per passione e professione faccio il personal trainer. Nel mio passato ho fatto molti sport: karate, calcio, kung fu, pallavolo, golf e il tennis che tutt’ora è una grande passione."
+            }
+          />
+          <br />
+          <Typography
+            className={styles.text}
+            variant={"paragraph"}
+            label={
+              "Una sera del 2012 un amico mi mostrò un video di Frank Medrano, quel giorno scoprì la passione per il Calisthenics. Rimasi così impressionato da mollare immediatamente la sala pesi, il giorno dopo alle 8:00 di mattina ero già al parco per fare il mio primo allenamento."
+            }
+          />
+
+          <Button
+            className={styles.button}
+            label={"Leggi la mia storia"}
+            type={"primary"}
+          />
         </div>
       </SectionContainer>
-      <div className={styles.certificatesScrollableContainer}>
-        <div className={styles.certificatesContainer}>
-          <Typography
-            style={{ marginBottom: 24 }}
-            variant={"heading"}
-            label={"Attestati"}
-          />
-          <div className={styles.certificatesContentContainer}>
-            <div className={styles.singleCertificateContainer}>
-              <div className={styles.certificateImgContainer}>
-                <img
-                  src={attestatoAICS}
-                  alt={"Logo Attestato Istruttore Calisthenico (AICS)"}
-                />
-              </div>
-              <Typography
-                className={styles.certificateText}
-                variant={"smallTitle"}
-                label={"Istruttore Allenamento Calisthenico (AICS)"}
-              />
-            </div>
-            <div className={styles.singleCertificateContainer}>
-              <div className={styles.certificateImgContainer}>
-                <img
-                  style={{ height: 101, width: 101 }}
-                  src={attestatoFGI}
-                  alt={"Logo federazione Ginnastica Italiana"}
-                />
-              </div>
-              <Typography
-                className={styles.certificateText}
-                variant={"smallTitle"}
-                label={"Master “L’ ABC della GINNASTICA”"}
-              />
-            </div>
-            <div className={styles.singleCertificateContainer}>
-              <div className={styles.certificateImgContainer}>
-                <img
-                  src={attestatoTrainingLab}
-                  alt={"Logo Training Lab Italia"}
-                />
-              </div>
-              <Typography
-                className={styles.certificateText}
-                variant={"smallTitle"}
-                label={"Istruttore Functional Training"}
-              />
-            </div>
-            <div className={styles.singleCertificateContainer}>
-              <div className={styles.certificateImgContainer}>
-                <img src={attestatoMidea} alt={"Logo Midea"} />
-              </div>
-              <Typography
-                className={styles.certificateText}
-                variant={"smallTitle"}
-                label={"Istruttore Posturologia"}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Certificates />
     </div>
   );
 };
