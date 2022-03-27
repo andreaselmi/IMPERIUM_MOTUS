492f65f25abca040abcc0de237ab52f963b3b20b
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-27 12:38:07 +0200

    add desktop navbar animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar/Navbar";
 import CalisthenicsSection from "./components/Section/CalisthenicsSection/CalisthenicsSection";
-import Grid from "./components/Grid/Grid";
 
 function App() {
   return (
--- src/components/Navbar/NavbarDesktop/NavbarDesktop.tsx
+++ src/components/Navbar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./NavbarDesktop.module.css";
 import Typography from "../../Typography/Typography";
 import Container from "../../Container/Container";
@@ -7,46 +7,67 @@ import logo from "../../../assets/icons/logo.svg";
 import logoName from "../../../assets/icons/logoName.svg";
 
 const NavbarDesktop = () => {
+  const [compact, setCompact] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY == 0) {
+      setCompact(false);
+    } else {
+      setCompact(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      onScroll();
+    });
+
+    return window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div className={styles.navbarContainer}>
+    <div
+      className={`${styles.navbarContainer} ${compact && styles.compactNavbar}`}
+    >
       <Container style={{ display: "flex", flex: 1 }}>
         <div className={styles.navbar}>
           <a>
             <Typography
-              style={{ color: "white" }}
+              className={styles.navbarMenu}
               variant={"menu"}
               label={"IL CALISTHENICS"}
             />
           </a>
           <a>
             <Typography
-              style={{ color: "white" }}
+              className={styles.navbarMenu}
               variant={"menu"}
               label={"IL COACH"}
             />
           </a>
           <div className={styles.logoContainer}>
             <img
-              style={{ width: "75%" }}
+              className={`${styles.logo} ${compact && styles.compactLogo}`}
               src={logo}
               alt={"Logo di Imperium Motus"}
             />
+
             <img
-              style={{ width: "75%" }}
+              className={`${styles.logo} ${compact && styles.compactLogoName}`}
               src={logoName}
               alt={"Scritta Imperium Motus"}
             />
           </div>
           <a>
             <Typography
-              style={{ color: "white" }}
+              className={styles.navbarMenu}
               variant={"menu"}
               label={"I CORSI"}
             />
           </a>
           <a>
             <Typography
-              style={{ color: "white" }}
+              className={styles.navbarMenu}
               variant={"menu"}
               label={"DOVE SIAMO"}
             />
