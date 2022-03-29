bbdae1c6c7820f4e107e109a771c8f2ef62b180b
Andrea Selmi <andreaselmi90@gmail.com>
2022-03-29 16:24:10 +0200

    add responsive height to map

--- src/components/Sections/MapSection/MapSection.tsx
+++ src/components/Sections/MapSection/MapSection.tsx
@@ -21,9 +21,7 @@ const MapSection = () => {
 
       <div className={styles.mapContainer}>
         <iframe
-          style={{ filter: "invert(90%)" }}
-          width={"100%"}
-          height={500}
+          className={styles.map}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3041.7105615336886!2d18.17906381628472!3d40.326580779375824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13442f72df4eea97%3A0xdc63e72b808839d5!2sVia%20dei%20Bersaglieri%2C%2024%2C%2073020%20Castromediano%20LE!5e0!3m2!1sit!2sit!4v1648493116191!5m2!1sit!2sit"
           allowFullScreen={true}
           loading="lazy"
