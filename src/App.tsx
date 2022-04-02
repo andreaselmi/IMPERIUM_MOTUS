5fdf8c957753a8c8946d59739134f1a2728ebe87
Andrea Selmi <andreaselmi90@gmail.com>
2022-04-02 15:53:59 +0200

    m

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar/Navbar";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
-import ScheduleSlot, {
-  scheduleSlotTime,
-} from "./components/ScheduleSlot/ScheduleSlot";
 
 function App() {
   return (
