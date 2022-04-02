f6b959410a06d28359549b719e3e26e72543c8a7
Andrea Selmi <andreaselmi90@gmail.com>
2022-04-02 14:23:13 +0200

    add slot schedule component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar/Navbar";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
+import ScheduleSlot, {
+  scheduleSlotTime,
+} from "./components/ScheduleSlot/ScheduleSlot";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Header />
-      <Body />
-      <Footer />
+      <ScheduleSlot slotTime={scheduleSlotTime.hourHalf} />
+      {/*<Navbar />*/}
+      {/*<Header />*/}
+      {/*<Body />*/}
+      {/*<Footer />*/}
     </>
   );
 }
---
+++ src/components/ScheduleSlot/ScheduleSlot.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export enum scheduleSlotTime {
+  half = 30,
+  hour = 60,
+  hourHalf = 90,
+  twoHour = 120,
+  twoHalfHour = 150,
+}
+
+interface ScheduleSlotProps {
+  slotTime: scheduleSlotTime;
+}
+
+const ScheduleSlot = ({ slotTime }: ScheduleSlotProps) => {
+  const checkHeight = (val: scheduleSlotTime) => {
+    if (val === 30) {
+      return val;
+    }
+
+    const halfNumber = val / 30;
+    const totalTimeHeight = 26 * halfNumber;
+    return totalTimeHeight + (halfNumber - 1) * 3;
+  };
+
+  return <div>{checkHeight(slotTime)}</div>;
+};
+
+export default ScheduleSlot;
