a0145b281cb3dde1090bb45fae8f4581e00f17c3
Andrea Selmi <andreaselmi90@gmail.com>
2022-04-04 16:36:11 +0200

    .

--- src/components/Header/HeaderLogo/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo/HeaderLogo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./HeaderLogo.module.scss";
 
+import { ReactComponent as Calisthenics } from "../../../assets/images/header/header2.svg";
+
 import header1 from "../../../assets/images/header/header1.svg";
 import header2 from "../../../assets/images/header/header2.svg";
 import header3 from "../../../assets/images/header/header3.svg";
@@ -12,7 +14,8 @@ const HeaderLogo = () => {
         <img className={styles.logo1} alt={"Il primo studio"} src={header1} />
       </div>
       <div className={styles.logo2Container}>
-        <img className={styles.logo2} alt={"Calisthenics"} src={header2} />
+        <Calisthenics className={styles.logo2} />
+        {/*<img  alt={"Calisthenics"} src={header2} />*/}
       </div>
       <div className={styles.logo3Container}>
         <img className={styles.logo3} alt={"A Lecce"} src={header3} />
