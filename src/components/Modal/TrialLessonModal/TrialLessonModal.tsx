9b2df9c7dbace2dca5e521c0c2f8927121b267fa
Andrea Selmi <andreaselmi90@gmail.com>
2022-05-06 11:05:43 +0200

    add new url to email handler

--- src/components/Modal/TrialLessonModal/TrialLessonModal.tsx
+++ src/components/Modal/TrialLessonModal/TrialLessonModal.tsx
@@ -4,6 +4,8 @@ import styles from "./TrialLessonModal.module.scss";
 import Button from "../../Button/Button";
 import axios from "axios";
 
+const mailUrl = process.env.REACT_APP_SERVER_MAIL;
+
 const TrialLessonModal = () => {
   const [fname, setFname] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -11,16 +13,20 @@ const TrialLessonModal = () => {
 
   const onSubmittingForm = async (e: FormEvent) => {
     e.preventDefault();
-    try {
-      const res = await axios.post("http://localhost:3000/mail", {
-        name: fname,
-        phone: phoneNumber,
-        info,
-      });
+    if (mailUrl) {
+      try {
+        const res = await axios.post(mailUrl, {
+          name: fname,
+          phone: phoneNumber,
+          info,
+        });
 
-      console.log(res.data);
-    } catch (e) {
-      console.log(e, "errore");
+        console.log(res.data);
+      } catch (e) {
+        console.log(e, "errore");
+      }
+    } else {
+      console.log("L'invio non è disponibile");
     }
   };
 
