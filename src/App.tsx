9cec9e50c260951b1f608b63a7fbaf005ad094ef
Andrea Selmi <andreaselmi90@gmail.com>
2022-09-23 12:36:49 +0200

    add react-google-analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import ReactGA from "react-ga";
 
 function App() {
+  const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+  if (TRACKING_ID) {
+    ReactGA.initialize(TRACKING_ID);
+  }
+
   return (
     <Provider store={store}>
       <Navbar />
