ee3ca9719ef904b9b880fe942de64b98043745bd
Andrea Selmi <65682335+andreaselmi@users.noreply.github.com>
2022-10-07 19:52:36 +0200

    add storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Button from "./Button";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Button",
+  component: Button,
+  argTypes: {
+    type: {
+      options: ["primary", "secondary"],
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+export const Secondary = Template.bind({});
+
+Primary.args = {
+  type: "primary",
+  label: "Button",
+};
+
+Secondary.args = {
+  type: "secondary",
+  label: "Button",
+};
