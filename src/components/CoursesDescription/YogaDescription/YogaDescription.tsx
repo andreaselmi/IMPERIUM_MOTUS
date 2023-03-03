f6cd2d2c42940f95c16971a1ac5bf18ff56042b5
Andrea Selmi <65682335+andreaselmi@users.noreply.github.com>
2023-03-03 19:44:08 +0100

    changed yoga text in ourcoursessection

--- src/components/CoursesDescription/YogaDescription/YogaDescription.tsx
+++ src/components/CoursesDescription/YogaDescription/YogaDescription.tsx
@@ -13,48 +13,16 @@ const YogaDescription = ({ onClickButton }: YogaDescriptionProps) => {
     <>
       <section className={styles.courseDescriptionContainer}>
         <Typography className={styles.bodyText} variant={textVariant.paragraph}>
-          Conosciuto anche come Yoga della forza, agisce sia sul piano fisico
-          che su quello psichico-spirituale. Con posizioni semplici ed eseguite
-          con calma, unitamente a nozioni di respirazione, rilassamento e
-          meditazione, questo stile è perfetto per i principianti, ma viene
-          utilizzato anche dai più esperti per trovare un maggior equilibrio.
+          I problemi di postura stanno diventando sempre più frequenti a
+          qualsiasi età; secondo recenti studi il della popolazione mondiale
+          soffre di mal di schiena e non è così azzardato affermare che si
+          tratta di uno dei problemi principali dell’era moderna.
         </Typography>
         <br />
-        <ul>
-          <li className={styles.listItem}>
-            <Typography
-              className={styles.bodyText}
-              variant={textVariant.paragraph}
-            >
-              Previene mal di schiena, scoliosi edolori cervicali
-            </Typography>
-          </li>
-          <li className={styles.listItem}>
-            <Typography
-              className={styles.bodyText}
-              variant={textVariant.paragraph}
-            >
-              Mantiene mobile la colonna vertebrale, le articolazioni e il
-              sistema scheletrico e muscolare in generale
-            </Typography>
-          </li>
-          <li className={styles.listItem}>
-            <Typography
-              className={styles.bodyText}
-              variant={textVariant.paragraph}
-            >
-              Favorisce il controllo del proprio corpo
-            </Typography>
-          </li>
-          <li className={styles.listItem}>
-            <Typography
-              className={styles.bodyText}
-              variant={textVariant.paragraph}
-            >
-              Previene le vene varicose
-            </Typography>
-          </li>
-        </ul>
+        <Typography className={styles.bodyText} variant={textVariant.paragraph}>
+          Per questo lo yoga terapeutico è adatto per chiunque soffra di
+          squilibri posturali,e quindi si rivela perfetto per:
+        </Typography>
         <Button
           className={styles.modalButton}
           onClick={onClickButton}
--- src/components/Modal/ModalsContent/ModalYogaContent/ModalYogaContent.tsx
+++ src/components/Modal/ModalsContent/ModalYogaContent/ModalYogaContent.tsx
@@ -7,18 +7,78 @@ import { textVariant } from "../../../../defs/textVariant";
 const ModalYogaContent = () => {
   return (
     <>
-      <ModalHeader title={"Hatha yoga"} />
+      <ModalHeader title={"Yoga Posturale"} />
 
       <div className={styles.firstSectionText}>
         <Typography variant={textVariant.paragraph}>
-          Conosciuto anche come Yoga della forza, agisce sia sul piano fisico
-          che su quello psichico-spirituale. Con posizioni semplici ed eseguite
-          con calma, unitamente a nozioni di respirazione,rilassamento e
-          meditazione, questo stile è perfetto per i principianti, ma viene
-          utilizzato anche dai più esperti per trovare un maggior equilibrio.
+          È forse limitante parlare di yoga posturale perché, in effetti, tutto
+          lo yoga è adatto per mantenere una corretta postura. L’allenamento
+          all’ascolto del proprio corpo, alla concentrazione, alla
+          propriocezione e alla meditazione, infatti, permettono di portare ciò
+          che si impara e si sperimenta sul tappetino anche nella vita di tutti
+          i giorni; ciò comporta che le posizioni corrette apprese durante la
+          pratica diventano sempre più naturali e spontanee, così come diviene
+          sempre più semplice acquisire e mantenere una corretta igiene
+          posturale. Lo yoga posturale ha, però, il principale obiettivo di
+          correggere i problemi e gli squilibri posturali; possiamo considerarlo
+          una sorta di derivazione occidentale della pratica originaria, visto
+          che si concentra proprio su quelle posizioni che sfruttano i riflessi
+          neuro muscolari per ripristinare la condizione di equilibrio; in
+          questo modo è possibile combattere un torcicollo, risolvere un mal di
+          schiena, lenire il{" "}
+          <b style={{ fontWeight: "bold" }}>dolore cervicale</b> e, più in
+          generale, acquisire una condizione armonia e di benessere. Ovviamente
+          anche lo yoga posturale, sebbene abbia una vocazione più fisica,
+          mantiene anche la componente spirituale; non mancano infatti gli asana
+          dedicati al rilassamento e alla concentrazione e gli esercizi di
+          respirazione. Da aggiungere, per concludere, che lo yoga posturale si
+          rivela perfetto anche quando si deve recuperare la condizione fisica
+          dopo un incidente o un’operazione; possiamo quindi considerarlo una
+          forma base dello{" "}
+          <b style={{ fontWeight: "bold" }}>yoga terapeutico</b>.
         </Typography>
       </div>
       <div className={styles.secondSectionText}>
+        <Typography variant={textVariant.heading}>
+          Yoga posturale: a chi è adatto
+        </Typography>
+        <Typography variant={textVariant.paragraph} style={{ marginTop: 16 }}>
+          I problemi di postura stanno diventando sempre più frequenti a
+          qualsiasi età; secondo recenti studi il della popolazione mondiale
+          soffre di mal di schiena e non è così azzardato affermare che si
+          tratta di uno dei problemi principali dell’era moderna.
+        </Typography>
+        <br />
+        <Typography variant={textVariant.paragraph}>
+          Per questo lo yoga terapeutico è adatto per chiunque soffra di
+          squilibri posturali, e quindi si rivela perfetto per:
+        </Typography>
+        <ul className={styles.list}>
+          <li>
+            <Typography variant={textVariant.paragraph}>
+              i giovani che passano troppo tempo seduti a una scrivania;
+            </Typography>
+          </li>
+          <li>
+            <Typography variant={textVariant.paragraph}>
+              gli anziani che hanno le articolazioni un po’ usurate e
+              irrigidite;
+            </Typography>
+          </li>
+          <li>
+            <Typography variant={textVariant.paragraph}>
+              chiunque passa troppo tempo in piedi per lavoro ed è costretto ad
+              assumere posizioni scorrette durante la giornata;
+            </Typography>
+          </li>
+          <li>
+            <Typography variant={textVariant.paragraph}>
+              chi è dovuto stare fermo per tanto tempo e sta affrontando un
+              percorso riabilitativo.
+            </Typography>
+          </li>
+        </ul>
+
         <Typography variant={textVariant.heading}>I benefici</Typography>
 
         <ul className={styles.list}>
